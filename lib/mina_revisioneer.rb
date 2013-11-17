f7b1f8217eaeb5b0146bce977bf12e2f6fd96e87
Raphael Randschau <nicolai86@me.com>
2013-11-17 14:42:53 +0100

    first attempt at proper revisioneer interfacing

--- lib/mina_revisioneer.rb
+++ lib/mina_revisioneer.rb
@@ -1,4 +1,5 @@
 require "mina_revisioneer/version"
+require "json"
 
 # # Modules: Revisioneer
 # Adds settings and tasks for interfacing with Revisioneer
@@ -18,6 +19,12 @@ set_default :revisioneer_host, 'https://revisioneer.io'
 
 set_default :revisioneer_api_token, ''
 
+set_default :revisioneer_sha, %x[git rev-parse --verify HEAD].strip
+
+set_default :revisioneer_last_deploy_date, JSON.parse(%x[curl "#{revisioneer_host}/deployments?limit=1" -H "API-TOKEN: #{revisioneer_api_token}"].strip || '{}')["deployed_at"]
+
+set_default :revisioneer_messages, %x[git log --pretty=format:'%s' --abbrev-commit --since '#{revisioneer_last_deploy_date}'].strip.lines
+
 # ## Deploy tasks
 # These tasks are meant to be invoked inside deploy scripts, not invoked on
 # their own.
@@ -29,10 +36,7 @@ namespace :revisioneer do
   task :notify do
     queue %{
       echo "-----> Notifying revisioneer"
-      #{echo_cmd %[mkdir -p "#{deploy_to}/#{shared_path}/bundle"]}
-      #{echo_cmd %[mkdir -p "#{File.dirname bundle_path}"]}
-      #{echo_cmd %[ln -s "#{deploy_to}/#{shared_path}/bundle" "#{bundle_path}"]}
-      #{echo_cmd %[#{bundle_bin} install #{bundle_options}]}
+      #{echo_cmd %[curl -X POST "#{revisioneer_host}/deployments" -d '{ "sha": "#{revisioneer_sha}", "messages": #{JSON.dump(revisioneer_messages)} }' -H "API-TOKEN: #{revisioneer_api_token}"]}
     }
   end
 end
