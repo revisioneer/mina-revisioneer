3e734292e3f508e101a97d8ce168400f4b02af63
Raphael Randschau <nicolai86@me.com>
2013-11-22 22:59:30 +0100

    use rugged to extract git informations

--- lib/mina/revisioneer_helpers.rb
+++ lib/mina/revisioneer_helpers.rb
@@ -1,30 +1,50 @@
 require "json"
+require "rugged"
 
 module Mina
   module RevisioneerHelpers
-    def last_deploy_date
-      curl = %Q{curl "#{revisioneer_host}/deployments?limit=1" -H "API-TOKEN: #{revisioneer_api_token}" -s}
-      response = %x[#{curl}].strip
-      Time.parse(JSON.parse(response).first.fetch("deployed_at"))
-    rescue
+    def last_deploy
+      @last_deploy ||= begin
+        curl = %Q{curl "#{revisioneer_host}/deployments?limit=1" -H "API-TOKEN: #{revisioneer_api_token}" -s}
+        response = %x[#{curl}].strip
+        JSON.parse(response).first
+      end
+    rescue => err
+      puts err
       # no JSON received - first deploy?
-      ""
+      {}
+    end
+
+    def last_deploy_date
+      Time.parse(last_deploy.fetch("deployed_at"))
+    end
+
+    def last_deploy_sha
+      last_deploy.fetch("sha")
+    end
+
+    def repo
+      @repo ||= Rugged::Repository.new(".")
     end
 
     def deploy_messages
-      if last_deploy_date != ""
-        messages = %x[git log --pretty=format:'%s' --abbrev-commit --since '#{last_deploy_date}']
-      else
-        messages = %x[git log --pretty=format:'%s' --abbrev-commit]
-      end
-      messages = messages.strip.lines.map(&:strip)
+      walker = Rugged::Walker.new(repo)
+      walker.push current_sha
+      walker.hide last_deploy_sha
+      messages = walker.each.to_a.map(&:message)
       messages.select! { |line| line =~ revisioneer_inclusion } if revisioneer_inclusion
       messages.reject! { |line| line =~ revisioneer_exclusion } if revisioneer_exclusion
       messages
     end
 
     def current_sha
-      %x[git rev-parse --verify HEAD].strip
+      ref = repo.head
+      ref.target
     end
   end
 end
+
+__END__
+require "rugged"
+repo = Rugged::Repository.new(".")
+commit = Rugged::Commit.lookup repo, repo.head.target
